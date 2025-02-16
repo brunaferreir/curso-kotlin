f8516cc3be633c81eb9c5d35e95087955caddb1c
Bruna <bruna.fsilva@aluno.faculdadeimpacta.com.br>
2025-02-16 19:04:54 -0300

    operadores

---
+++ src/OperadoresLogicos.kt
@@ -0,0 +1,20 @@
+/*
+     != verifica se é diferente de
+     == verifica se e igual a
+     ! inversão de estado
+     is (intanceof) verifica se um tipo e igual a outro tipo
+ */
+
+fun main(args:Array<String>){
+    val x = 2
+    val y = 2
+    val z = "zxc"
+    println((x != y))
+    println(x == y)
+
+    println(x is Int)  //Verifica o tipo do dado
+    val b = false
+
+    println(b)
+
+}
