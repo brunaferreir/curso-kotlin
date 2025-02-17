d14cb7ec75adcfecbf55321e37e425d5fa7d8fee
bruna ferreira <brunaferreira0320@gmail.com>
2025-02-17 09:44:22 -0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -13,3 +13,42 @@ fun main() {
 
 
 }
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var txt = "Eu AMO kotlin"
+    println(txt[0])
+    println(txt[2])
+
+    var txt2 = "Hoje é segunda, bora segundar!"
+    println("Tamanho: ${txt2.length}")
+    //println("Tamanho: +{txt2.length}")
+
+    var txt3 = "palmeiras não tem mundial"
+    println(txt3.uppercase())
+
+    var txt4 = "PALMEIRAS NÃO TEM MUNDIAL"
+    println(txt3.lowercase())
+
+    var txt5 = "Eu AMO kotlin"
+    var txt6 = "Eu AMO kotlin"
+    println(txt5.compareTo(txt6))
+
+    var txt7 = "Please locate where 'locate' occurs!"
+    println(txt7.indexOf("locate"))
+
+    // Concatenação de String +
+
+    var firstName = "Adam"
+    var lastName = "Sandler"
+    println("Olá, eu sou "+firstName+" "+lastName)
+
+    // Interpolação de String
+
+    var firstName2 = "Mike"
+    var lastName2 = "Wazowski"
+    println("My name is $firstName2 $lastName2")
+    println("O nome Mike tem ${firstName2.length} letras" )
+    println("O sobrenome Wazowski tem ${lastName2.length}")
+}
