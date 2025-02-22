5041c4928290115fd5685a327eb72175274e958e
Bruna <bruna.fsilva@aluno.faculdadeimpacta.com.br>
2025-02-22 20:10:37 -0300

    ok

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,3 @@
-fun main() {
-    val name = "Bruna"
-    val name2 = "Ferreira"
-    var peso = 57
-    var altura = 1.67
-    var possuiAviao = "sim"
-
-    println("Nome: $name")
-    println("Sobrenome: $name2")
-    println("Peso: $peso")
-    println("Altura $altura")
-    println("Possui avião? $possuiAviao")
-
-
-}
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
